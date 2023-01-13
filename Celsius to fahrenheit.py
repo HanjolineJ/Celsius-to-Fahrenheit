3ae1e25fc98b60bd4cd8c9183c0abaa5c957c216
HanjolineJ <122487862+HanjolineJ@users.noreply.github.com>
2023-01-13 18:36:59 -0500

    Add files via upload

---
+++ Celsius to fahrenheit.py
@@ -0,0 +1,6 @@
+print; cel = float(input(" Enter celsius temperature: ")) 
+# using a float we can ask the user to enter the celsius temperature
+fahrenheit = (cel * 9/5) + 32
+#using the float for a conversion frome celsius to  fahrenheit
+print ( ' Your  fahrenheit temperture is ' ,  fahrenheit  )
+# now after using the float as a conversion too we can print  fahrenheit from celsius
